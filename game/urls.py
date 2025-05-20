1049503fb3be61fd41e007a95f5dcefadeb87a7c
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-05-20 09:05:55 +0800

    urls with slash

--- game/urls.py
+++ game/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', main, name='main'),
     path('claw_machine/', claw_machine, name='claw_machine'),
     path('submit-score/', submit_score, name='submit_score'),
-    path('prize', prize, name='prize'),
-    path('feedback', feedback, name='feedback'),
+    path('prize/', prize, name='prize'),
+    path('feedback/', feedback, name='feedback'),
     path('submit-feedback/', submit_feedback, name='submit_feedback'),
     path('upload-prizes/', upload_prizes, name='upload_prizes'),
 ]
