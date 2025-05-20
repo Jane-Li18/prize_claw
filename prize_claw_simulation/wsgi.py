9752db89aa55ae16414c3ee06694a684d57a80d7
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-05-20 10:37:50 +0800

    edit wsgi again

--- prize_claw_simulation/wsgi.py
+++ prize_claw_simulation/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prize_claw_simulation.settings')
 
-app = get_wsgi_application()
+# Define the WSGI application callable
+application = get_wsgi_application()
+
+# If you want to use 'app' locally, you can create a reference
+# but keep 'application' as the main callable for Vercel
+app = application
