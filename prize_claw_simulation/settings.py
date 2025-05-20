7197872f9310f7d219394f21d328c04a9c9d1d49
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-05-20 14:29:34 +0800

    supabase connect?

--- prize_claw_simulation/settings.py
+++ prize_claw_simulation/settings.py
@@ -94,11 +94,13 @@ WSGI_APPLICATION = 'prize_claw_simulation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres.dusbhirtbgkfkzldqoob:EsLee08182002#@aws-0-us-east-2.pooler.supabase.com:6543/postgres'
+    )
 }
 
 
